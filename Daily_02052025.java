d8cdf3664b37885d5898edf13b274feb7f049a68
Adi <adityabeborta@gmail.com>
2025-05-02 12:31:23 +0530

    day 4 challenge - comparator

---
+++ Daily_02052025.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class MyComparator implements Comparator<Integer> {
+    public int compare(Integer i1, Integer i2) {
+        return i2.compareTo(i1);
+    }
+}
+
+public class Daily_02052025 {
+//create an arraylist of Integers and sort it in ascending order as well as in descending order
+
+    public static void main(String[] args) {
+        List<Integer> originalList = new ArrayList<>();
+        originalList.add(25);
+        originalList.add(0);
+        originalList.add(5);
+        originalList.add(20);
+        originalList.add(10);
+        System.out.println("original list " + originalList);
+        //using collections.sort for natural sorting order which is ascending by default
+        Collections.sort(originalList);
+        //it internally uses compareTo method
+        System.out.println("Ascending order sorting - NSO " + originalList);
+        //for customized sorting order we can use comparator
+        Collections.sort(originalList, new MyComparator());
+        System.out.println("Descending order sorting - DSO " + originalList);
+
+    }
+}
